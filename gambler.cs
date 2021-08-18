578fa6a91c4ba3dd0f2e72650ce35d06d6382da6
syedfaizz <syedfaizz45@gmail.com>
2021-08-18 10:52:43 +0530

    [faisal] Add: uc2-win-loss

--- gambler.cs
+++ gambler.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace Gambling_simulation
 {
-    public class gambler
+    public class Gambler
     {
         const int STAKE_PER_DAY = 100;
         const int BET_PER_GAME = 1;
+        const int WIN = 1;
+        public static void GameStatus()
+        {
+            Random random = new Random();
+            int stake = random.Next(1, 3);
+            if (stake == WIN)
+                Console.WriteLine("Playe win the game");
+            else
+                Console.WriteLine("Playe loss the game");
+        }
     }
 }
