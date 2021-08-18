f6e5ecf066dda677701c60274a7e52a6775d530b
syedfaizz <syedfaizz45@gmail.com>
2021-08-18 09:56:22 +0530

    [faisal] Add: uc1-start-gambling

---
+++ gambler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling_simulation
+{
+    public class gambler
+    {
+        const int STAKE_PER_DAY = 100;
+        const int BET_PER_GAME = 1;
+    }
+}
